6ee8c9052541136398eef6c74b106b84a149fff7
ninii <wuy0951@gmail.com>
2024-09-04 11:42:22 +0800

    new swiper

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 import './assets/scss/all.scss';
-
-console.log("Hello world!");
+const swiper = new Swiper(".mySwiper", {
+   slidesPerView: 1,
+   slidesPerGroup:2,
+   // ***(一次呈現幾張),
+   // ***(一個群組內有幾張投影片),
+   spaceBetween: 48,
+   // ***(分頁),
+   pagination: {
+     el:".swiper-pagination-custom",
+     type: "fraction"
+   },
+   navigation: {
+         nextEl: ".swiper-next",
+         prevEl: ".swiper-prev",
+       },
+ });
